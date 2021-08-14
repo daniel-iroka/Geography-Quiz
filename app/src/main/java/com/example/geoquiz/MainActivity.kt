67b75a542b8d13366a27731760b6e98ffd67083b
daniel-iroka <danieliroka67@gmail.com>
2021-08-14 15:27:33 +0100

    added lifecycle callbacks

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"  // TAG constant refers to the source of the log message(The Activity class)
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton : Button  // This are all properties that will be used to hold the Views
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton :Button
     private lateinit var questionTextView :TextView
 
-    // TODO : Continue to referencing Resources in xml.....
+    // todo continue from creating a landscape layout - Device Configuration...
 
     // This is a list holding all the questions being passed to the 'Question Class' as a StringResourceId and a Boolean
     private val questionBank = listOf(
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")  // log message shown based on the current state of the Activity
         setContentView(R.layout.activity_main) // this is an auto-generated resource "id" for the layout
 
         trueButton = findViewById(R.id.true_button)  // This are all properties identifying the views by ids in the MainActivity.kt
@@ -60,6 +64,32 @@ class MainActivity : AppCompatActivity() {
         }
         showAllQuestions() // This is added here to so that the first Question will initially appear in the questionsTextView
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
 
     // This function will hold all the questionText
     private fun showAllQuestions() {
